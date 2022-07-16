4ad0ceb9b8ab23b4867bdf217cd27cd6bf34fe8f
ChunWun <94582200+ChunWun@users.noreply.github.com>
2022-07-17 01:44:45 +0800

    add drop down button

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from "./Header.module.css";
 import { Box } from '@mui/material';
 import imgCode from "../../asset/CodeBackground.png";
@@ -8,41 +8,52 @@ import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
 
 const Header = (props) => {
+	const [isDropDownHit, setIsDropDownHit] = useState(false);
+
+	const dropDownButtonStyle = (isDropDownHit) ? styles.dropDownButtonOn : styles.dropDownButtonOff;
+	const linkboxStyle = (isDropDownHit) ? styles.linkBoxDrop : styles.linkBox;
+
+	const onDropDownHitHandler = () => {
+		setIsDropDownHit(!isDropDownHit);
+	}
+
 	return (
 		<React.Fragment>
 			<header className={styles.header}>
 				<h1>Jim's&nbsp;CV</h1>
-				<Box className={styles.box}>
-					<a href='https://www.facebook.com/chunwun.huang/' target='_blank' rel="noreferrer">
-						<div className={styles.icon}>
-							<FacebookIcon fontSize="large" color='info' />
-						</div>
-					</a>
 
-					<a href='https://www.instagram.com/chunwun.huang/' target='_blank' rel="noreferrer">
-						<div className={styles.icon}>
-							<InstagramIcon fontSize="large" color='info' />
-						</div>
-					</a>
-					{/* https://github.com/ChunWun */}
-					<a href='' target='_blank' rel="noreferrer">
-						<div className={styles.icon}>
-							<GitHubIcon fontSize="large" color='info' />
-						</div>
-					</a>
+				<div className={styles.box}>
+					<button className={dropDownButtonStyle} onClick={onDropDownHitHandler}>+</button>
+					<Box className={linkboxStyle}>
+						<a href='https://www.facebook.com/chunwun.huang/' target='_blank' rel="noreferrer">
+							<div className={styles.icon}>
+								<FacebookIcon fontSize="large" color='warning' />
+							</div>
+						</a>
 
-					<a href='https://www.linkedin.com/in/jim-huang-397846190/' target='_blank' rel="noreferrer">
-						<div className={styles.icon}>
-							<LinkedInIcon fontSize="large" color='info' />
-						</div>
-					</a>
+						<a href='https://www.instagram.com/chunwun.huang/' target='_blank' rel="noreferrer">
+							<div className={styles.icon}>
+								<InstagramIcon fontSize="large" color='warning' />
+							</div>
+						</a>
+						<a href='' target='_blank' rel="noreferrer">
+							<div className={styles.icon}>
+								<GitHubIcon fontSize="large" color='warning' />
+							</div>
+						</a>
 
-				</Box >
+						<a href='https://www.linkedin.com/in/jim-huang-397846190/' target='_blank' rel="noreferrer">
+							<div className={styles.icon}>
+								<LinkedInIcon fontSize="large" color='warning' />
+							</div>
+						</a>
+					</Box>
+				</div >
 			</header>
 			<div className={styles.mainImage}>
 				<img src={imgCode} alt='A table full of delicious food!' />
 			</div>
-		</React.Fragment>
+		</React.Fragment >
 	)
 }
 
